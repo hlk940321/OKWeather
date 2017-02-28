895e6bde4ff3bd9d361441c57952e8bca4789496
华凌凯 <453223492@qq.com>
2017-03-01 00:02:58 +0800

    创建了province,city,country的表和表的各项配置

---
+++ app/src/main/java/com/okweather/android/okweather/db/Country.java
@@ -0,0 +1,46 @@
+package com.okweather.android.okweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by hlk on 2017/2/28.
+ */
+
+public class Country extends DataSupport {
+    private int id;
+    private String countryName;
+    private String weatherId;
+    private int CityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return CityId;
+    }
+
+    public void setCityId(int cityId) {
+        CityId = cityId;
+    }
+}
